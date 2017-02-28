d3671785444bbbb63948affd002bc337ea7a4d86
hamt3ch <hmiles23@ufl.edu>
2017-02-28 11:47:21 -0800

    added word count problem

---
+++ randomCodingProblem/wordCount.py
@@ -0,0 +1,35 @@
+import sys
+
+def countWords(sent):
+    # init variables
+    i = 0
+    count = 0
+
+    # get first starting word
+    while(sent[i] == " "):
+        i += 1
+
+    # make sure index is still in range of str
+    while(i < len(sent)):
+
+        # move till you get a space
+        while i < len(sent) and sent[i] != " ":
+            i += 1
+
+        # count++ (found word)
+        count += 1
+
+        # move till you get letter
+        while i < len(sent) and sent[i] == " ":
+            i += 1
+
+    return count
+
+s = [
+    "this is a dog",
+    " i hate interviews so much ",
+    "  must  studyyyy  more  "
+    ]
+
+for item in s:
+    print(countWords(item))
