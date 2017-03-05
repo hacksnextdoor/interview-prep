23205cb36702ecb6d0db24da018c746e9043298e
hamt3ch <hmiles23@ufl.edu>
2017-03-04 23:45:11 -0800

    update binary search

--- Search&Sorting/binary_search.py
+++ Search&Sorting/binary_search.py
@@ -6,19 +6,19 @@ Binary Search Algorithm
 
 
 def binary_search(arr, x):
-    if not arr:
-        return None
+    if len(arr) == 1:
+        return arr[0] == x
 
     if arr[len(arr)/2] == x:
         return True
 
     if x > arr[len(arr)/2]:
-        return binary_search(arr[len(arr)//2:], x)
+        return binary_search(arr[len(arr)/2:], x)
 
-    return binary_search(arr[:len(arr)//2], x)
+    return binary_search(arr[:len(arr)/2], x)
 
 
 myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for i in range(1,10):
+for i in range(1,20):
     print(binary_search(myArr, i))
