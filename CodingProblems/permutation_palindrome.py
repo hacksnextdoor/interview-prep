4128f7f16a4d6371a575eaca4b585651eaaa6275
hamt3ch <hmiles23@ufl.edu>
2017-03-06 13:31:26 -0800

    fixed missed condition

--- CodingProblems/permutation_palindrome.py
+++ CodingProblems/permutation_palindrome.py
@@ -1,15 +1,13 @@
 """
-Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome ↴ .
+Write an efficient function that checks whether any permutation of an input string is a palindrome
 You can assume the input string only contains lowercase letters.
 
 Examples:
-
 "civic" should return True
 "ivicc" should return True
 "civil" should return False
 "livci" should return False
 """
-
 
 def str_to_map(stri):
     dict = {}
@@ -25,9 +23,8 @@ def permutation_palindrome(stri):
     mapStr = str_to_map(stri)
     odd_values = 0
     for key, value in mapStr.items():
-        if value % 2 == 0:
+        if value % 2 != 0:
             odd_values += 1
-
     if len(stri) % 2 == 0: # even values
         return odd_values == 0
     return odd_values == 1 # odd
@@ -39,9 +36,9 @@ test = {
     "civil": False,
     "livci": False,
     "ccccccccc": True,
-    "aaabbbcccc": True
+    "aaabbbcccc": False
 }
 
 for test, result in test.items():
-    if permutation_palindrome(test) == result:
+    if permutation_palindrome(test) != result:
         print("error w/ " + test)
